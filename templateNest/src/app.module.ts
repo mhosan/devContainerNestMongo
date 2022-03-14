5c93137d175e0be133465612a3545fcf826d45fa
Sergio Marcelo Hosan <smhosan@serv.externos.prismamp.com>
2022-03-14 14:55:19 -0300

    ajustando la db

--- templateNest/src/app.module.ts
+++ templateNest/src/app.module.ts
@@ -3,9 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoClient } from 'mongodb';
 
+//----testear conexión ----
 const uri ="mongodb+srv://admin:Ostruca1203@cluster0.rsnsq.mongodb.net/datosprueba?authSource=admin&replicaSet=atlas-z5nctz-shard-0&w=majority&readPreference=primary";
 const client = new MongoClient(uri);
-
 async function run(){
   await client.connect();
   const database = client.db('datosprueba');
@@ -14,7 +14,7 @@ async function run(){
   console.log(result);
 }
 run();
-
+//----testear conexión ----
 
 @Module({
   imports: [],
